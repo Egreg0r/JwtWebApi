3063d1a6ca3fb8eec5823f5b34e808443a3db445
Egreg0r <maximmus2510@gmail.com>
2021-12-09 16:28:01 +1000

    Add project files.

---
+++ JwtWebApi/Model/Ticket.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtWebApi.Model
+{
+    public class Ticket:BaseEntity
+    {
+        public string message { get; set; }
+        public LoginModel loginModel { get; set; }
+    }
+}
---
+++ JwtWebApi/Services/UserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JwtWebApi.Model;
+
+namespace JwtWebApi.Services
+{
+    public class UserService
+    {
+        public bool IsValidUserInformation(LoginModel model)
+        {
+            if (model.UserName.Equals("Jay") && model.Password.Equals("123456")) return true;
+            else return false;
+        }
+
+    }
+}
